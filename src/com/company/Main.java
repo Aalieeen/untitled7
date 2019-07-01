90d1bf385066bc5f973dcbedc0e30a531884a392
1 <alexander.pavlov_a@s.ru>
2019-07-01 12:48:10 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+import ss.Bass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bass b = new Bass();
+        b.foo();
+
+    }
+}
---
+++ src/ss/Bass.java
@@ -0,0 +1,16 @@
+package ss;
+
+public class Bass {
+    int a =5, b=7, c;
+    String str1 = "Hot", str2  = " java", result;
+
+
+    public void foo(){
+        c = a+b*a;
+        boolean q = a<b;
+        result = str1+str2;
+        System.out.println(q);
+        System.out.println(result); 
+    }
+    }
+
